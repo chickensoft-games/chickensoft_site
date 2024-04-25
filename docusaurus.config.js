dddfbeb6b4ebadb42261dbad065910f33eadd731
Joanna May <joannamadisonmay@gmail.com>
2024-04-24 22:24:24 -0500

    fix: formatting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -150,7 +150,7 @@ const config = {
               {
                 label: 'Open Source License',
                 href: '/license',
-              }
+              },
             ],
           },
           {
